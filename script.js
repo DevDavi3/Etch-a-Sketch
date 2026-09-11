3f5019d35f12faa708c8cd3cd35dced7c4d472c4
DevDevi3 <devdavie256@gmail.com>
2026-09-11 23:07:14 +0300

    feat: add a hover drawing function

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ const container = document.querySelector('.container');
 function createDivs(number) {
     for (let i = 0; i < number ; i++) {
         const div = document.createElement('div');
+        
         container.appendChild(div);
 
         for (let j = 0; j < number; j++)
         {
             const sq = document.createElement('div')
+            sq.classList.add('hover')
             div.appendChild(sq);
         }
     }
 }
 
 createDivs(16);
+const div = document.querySelectorAll('.hover')
+div.forEach((element)=>{
+    element.addEventListener('mouseenter', ()=>{
+        element.style.backgroundColor = 'red'
+    })
+})
+
+console.log(div)
