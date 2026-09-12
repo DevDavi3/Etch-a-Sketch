7a1ae043953074f7c52212dd69816106f64daf01
DevDevi3 <devdavie256@gmail.com>
2026-09-12 14:04:07 +0300

    feat: add a slider that increases the size of the grid

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 const container = document.querySelector('.container');
+const slider = document.querySelector('#slider');
 
 function createDivs(number) {
-    for (let i = 0; i < number ; i++) {
+
+    for (let i = 0; i < number; i++) {
+
         const div = document.createElement('div');
-        
+
         container.appendChild(div);
 
-        for (let j = 0; j < number; j++)
-        {
-            const sq = document.createElement('div')
-            sq.classList.add('hover')
+        for (let j = 0; j < number; j++) {
+
+            const sq = document.createElement('div');
+
+            sq.classList.add('hover');
+
             div.appendChild(sq);
         }
     }
 }
 
-createDivs(16);
-const div = document.querySelectorAll('.hover')
-div.forEach((element)=>{
-    element.addEventListener('mouseenter', ()=>{
-        element.style.backgroundColor = 'red'
-    })
-})
+function addHover() {
 
-console.log(div)
+    const div = document.querySelectorAll('.hover');
+
+    div.forEach((element) => {
+
+        element.addEventListener('mouseenter', () => {
+            element.style.backgroundColor = 'red';
+        });
+
+    });
+}
+
+let size = 16;
+
+createDivs(size);
+addHover();
+
+slider.addEventListener('change', (e) => {
+
+    container.innerHTML = "";
+
+    size = e.target.value;
+
+    createDivs(size);
+    addHover();
+});
